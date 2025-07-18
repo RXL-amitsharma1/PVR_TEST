39fc2f6fa5d74ca65ba0c6e6831326f922fee564
RXL-amitsharma1 <66236706+RXL-amitsharma1@users.noreply.github.com>
2025-07-18 14:35:58 +0530

    Update FormTagLibSpec.groovy

--- src/test/groovy/com/reports/FormTagLibSpec.groovy
+++ src/test/groovy/com/reports/FormTagLibSpec.groovy
@@ -7,6 +7,9 @@ import spock.lang.Specification
 class FormTagLibSpec extends Specification implements TagLibUnitTest<FormTagLib> {
 
     void "test localeSelect"() {
+
+
+        
         given:
         String attr = "name = 'test'"
         String formedTags = '<select name="test" id="test" >\r\n<option value="en" selected="selected" >English</option>\r\n<option value="ja" >Japanese</option>\r\n</select>'
